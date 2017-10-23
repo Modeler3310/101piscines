c58e80e0aa5987c6b421f945f270656409a92339
Guillaume Bayle <guibayle@z2r4p2.le-101.fr>
2017-10-23 16:16:12 +0200

    all depisotry

---
+++ Sastantua/ex00/sastantua.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sastantua.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guibayle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/08 11:12:51 by guibayle          #+#    #+#             */
+/*   Updated: 2017/10/08 16:38:50 by guibayle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+������� H__PAGEZERO�__TEXT__text__TEXT�
+^�
+�__stubs__TEXT
+�__stub_helper__TEXT
+$
+�__eh_frame__TEXT0�0�__DATA__nl_symbol_ptr__DATA__la_symbol_ptr__DATAH__LINKEDIT  �"�    @ �H!
+"�
+�!
+/usr/lib/dyld�[ ���>���(��J��$
+
+		*(��
+
+			8�/usr/lib/libSystem.B.dylib
+			x/usr/local/Cellar/gcc49/4.9-20140330/lib/gcc/x86_64-apple-darwin13.1.0/4.9.0/libgcc_s.1.dylib&� )!+!HUH��H����E�H�E�H�ƿ�A��UH��H���}�H�u�}�uH�E�H�H�H����������UH��H����u�E���E�������m��}���UH��}�u��E��E��E��1�E���E��E��m��}�E���������E�E��E�+E�;EċE�]�UH��H���}�u�E�ƿ�����U���ЉE�E���������E��E��E��5�E���E��m��m��}�E�����������)�E�E��E�+E�;E��E���UH��H��0�}܉u؉ЈEԋE�+E܉E�E���E�U؋E܉։��;����E�U؋E܉։�������E�E؃����E؃���E؉E��E��,�E�ƿ �h�����/�Y����}����E�+E�;E����E�������E�������)Ѓ��ƿ*�����}�~M�E�������U�)Ѓ�;E�u2�E���ƿ|�������$�������|�������E�ƿ|�����E�������E�������)Ѓ��ƿ*������E���ƿ*�p�����\�a����m��E��
+			������E��E�;E��������UH��H�� �}�E�E�}�%��}����ЋM�E�Ή��%����m��}����%
+			�%
+			L��AS�%��h�����h
+			�����zRx
+			�4R�������%�
+			zRx
+			1 �4'�������6�
+			zRx
+			$������)�
+			]T��������b�
+			��(���������
+					������������
+					;���������@�
+					&"R@dyld_stub_binderQr�r@_atoi�r@_write�__mh_execute_headerJft_putcharNmainSdisplay_Xcalc_qsastantua���sequencelfloor��width_top_floor�space_top_floot������%6)b����
+			D��
+			(
+			 libSystem.B�
+			 ,&
+			 B
+			 Q�
+			 c�
+			 o�
+			 ���
+			 @        __mh_execute_header_calc_space_top_floot_calc_width_top_floor_display_floor_display_sequence_ft_putchar_main_sastantua_atoi_writedyld_stub_binder%
---
+++ sudoku1/header.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guibayle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/15 07:26:39 by guibayle          #+#    #+#             */
+/*   Updated: 2017/10/15 20:25:57 by guibayle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+# include <unistd.h>
+# include <stdlib.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+void	ft_print_sudoku(char **grid);
+int		ft_solver(char **grid, int line, int col);
+#endif
